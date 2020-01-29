6f369bec248f96b53dd5decf78cc4da66c30dc9b
steinkorsveien <steinkorsveien@localhost>
2020-01-29 16:21:33 +0100

    Added Dish

--- functionalSystemTest/selenium/src/app.test.js
+++ functionalSystemTest/selenium/src/app.test.js
@@ -2,9 +2,14 @@ import getDriver from "./setupTests"
 import {getHostName} from "./setupTests"
 import {By, Key} from 'selenium-webdriver'
 
-it('should count when clicking', async () => {
-	 const driver = await getDriver()
+let driver
+beforeAll(async () => { 
+     driver = await getDriver()
 	 await driver.get(getHostName())
+})
+
+it('should count when clicking', async () => {
+	 
 	 let display = await driver.findElement(By.tagName('p'))
 	 expect(await display.getText()).toContain('You clicked 0 times')
 	 const button = await driver.findElement(By.tagName('button'))
@@ -12,4 +17,12 @@ it('should count when clicking', async () => {
 	 display = await driver.findElement(By.tagName('p'))
 	 expect(await display.getText()).toContain('You clicked 1 times')
 	 
+})
+
+xit('should enter value', async () => {
+   let input = await driver.findElement(By.tagName('input'))
+   expect(input.getAttribute('value')).toBe('')
+   input.sendKeys('Cool')
+   expect(input.getAttribute('value')).toBe('Cool')
+
 })
