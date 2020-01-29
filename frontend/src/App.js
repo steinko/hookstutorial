32dc6334c67e0783faa9c75d5416366fd1616007
steinkorsveien <steinkorsveien@localhost>
2020-01-29 16:34:50 +0100

    Reinit repo

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from './Counter' 
+import Dish from './Dish'
+
+function App() {
+  return (
+    <div>
+      <Counter />
+      <Dish />
+    </div>
+  )
+}
+
+export default App;
